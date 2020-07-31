2f3d0d98b76b337f0b6b9913e7c8e34dc19781dc
UET98er <35658884+UET98er@users.noreply.github.com>
2020-07-31 22:10:24 +0700

    upload week4

---
+++ Week4/src/week4/Week4.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package week4;
+
+/**
+ *
+ * @author QTV
+ */
+public class Week4 {
+    public static int max2Int(int a, int b) {
+        if(a>b){
+            return a;
+        }else{
+            return b;
+        }
+    }
+    
+    private int[] exArr = new int[100];
+    
+
+
+    
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
